eba065d6a6bd00b9bdf120d72fd9898e80f51655
heavymetalfan1123 <72998757+heavymetalfan1123@users.noreply.github.com>
2021-06-22 22:16:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
 
+
+<form>
+    <input type = "text" name = "name">
+    <input type = "text" name = "text">
+    <input type = "submit">
+</form>
+<?php
+
+class user{
+
+    public $name;
+    public $age;
+    public $sql;
+    public $text;
+
+
+    public function __construct(string $name,int $age,string $text,$sql )
+    {
+        $this ->name = $name;
+        $this ->age = $age; 
+        $this ->text = $text ;
+        
+        $this ->sql = $sql ;
+        $sql = mysqli_connect("localhost:3306", "root", "root","chat_bd");
+
+        if ($sql == false){
+            echo("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+        }
+        
+        $str = "INSERT INTO `db` (`name`, `text`) VALUES ('$name', '$text');";
+        
+        $result = mysqli_query($sql, $str);
+        
+        $data =  mysqli_query($sql,"SELECT * FROM `db`");
+        while ($row = mysqli_fetch_array($data)){
+            echo($row['name'].': '.$row['text'] ." <br>");
+        }
+        
+            
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+$name = $_GET["name"];
+$text = $_GET["text"];
+
+if (strlen($name)>0){
+
+    $user = new user($name,1,$text,"1");
+    
+
+}
+
+
+
+?>
