6aa48812420b5d9456aaa7b0b03f2538d0c71bc0
heavymetalfan1123 <72998757+heavymetalfan1123@users.noreply.github.com>
2021-06-22 22:33:32 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,66 +1,18 @@
-
-
-<form>
-    <input type = "text" name = "name">
-    <input type = "text" name = "text">
-    <input type = "submit">
-</form>
 <?php
 
-class user{
+declare(strict_types=1);
 
-    public $name;
-    public $age;
-    public $sql;
-    public $text;
+use PhpMyAdmin\Routing;
 
-
-    public function __construct(string $name,int $age,string $text,$sql )
-    {
-        $this ->name = $name;
-        $this ->age = $age; 
-        $this ->text = $text ;
-        
-        $this ->sql = $sql ;
-        $sql = mysqli_connect("chat-keke.herokuapp.com", "root", "root","chat_bd");
-
-        if ($sql == false){
-            echo("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-        }
-        
-        $str = "INSERT INTO `db` (`name`, `text`) VALUES ('$name', '$text');";
-        
-        $result = mysqli_query($sql, $str);
-        
-        $data =  mysqli_query($sql,"SELECT * FROM `db`");
-        while ($row = mysqli_fetch_array($data)){
-            echo($row['name'].': '.$row['text'] ." <br>");
-        }
-        
-            
-        
-    }
-    
-    
+if (! defined('ROOT_PATH')) {
+    // phpcs:disable PSR1.Files.SideEffects
+    define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+    // phpcs:enable
 }
 
+global $route, $containerBuilder;
 
+require_once ROOT_PATH . 'libraries/common.inc.php';
 
-
-
-
-
-
-$name = $_GET["name"];
-$text = $_GET["text"];
-
-if (strlen($name)>0){
-
-    $user = new user($name,1,$text,"1");
-    
-
-}
-
-
-
-?>
+$dispatcher = Routing::getDispatcher();
+Routing::callControllerForRoute($route, $dispatcher, $containerBuilder);
